5753296cefc9beb1814694b51213a4f4eff851bb
Jorge <jorge.silva8686@gmail.com>
2021-10-16 11:13:05 -0300

    Cambios funcionamiento online y puerto 17070

--- utilitarios/injector-lecturas-online.sh
+++ utilitarios/injector-lecturas-online.sh
@@ -5,5 +5,6 @@
     for linea in $(mysql -BN -u root -pBodenor848 -h 192.168.100.228 bodenor <<<"CALL SP_GET_CONVERSORES_COMANDO_API()"); do
         #echo $linea | sed 's/\t/;/g' | cut -d ";" -f7
         ./$linea
+        sleep 1
     done;
 )
